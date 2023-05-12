86cb956a307b4d6c5b0955f3eaa91727850e2d2d
Alan <alanampo@gmail.com>
2023-05-12 16:21:22 +0200

    primer commit

---
+++ config/modules.config.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * List of enabled modules for this application.
+ *
+ * This should be an array of module namespaces used in the application.
+ */
+return [
+    'Laminas\Paginator',
+    'Laminas\Navigation',
+    'Laminas\Mvc\Plugin\FilePrg',
+    'Laminas\Mvc\Plugin\FlashMessenger',
+    'Laminas\Mvc\Plugin\Identity',
+    'Laminas\Mvc\Plugin\Prg',
+    'Laminas\Session',
+    'Laminas\Mvc\Middleware',
+    'Laminas\Form',
+    'Laminas\I18n',
+    'Laminas\Log',
+    'Laminas\InputFilter',
+    'Laminas\Filter',
+    'Laminas\Hydrator',
+    'Laminas\Db',
+    'Laminas\Router',
+    'Laminas\Validator',
+    'Laminas\ZendFrameworkBridge',
+    'Laminas\Diactoros',
+    'Laminas\DeveloperTools',
+    'Application',
+    'Album',
+    "Blog"
+];
